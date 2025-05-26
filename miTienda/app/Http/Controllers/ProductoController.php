f69dad1921f823b329991bfc86df09f0de78ce6c
AlvaroDuran <adurama436@g.educaand.es>
2025-05-26 19:16:52 +0200

    ejercicio 2.2

---
+++ miTienda/app/Http/Controllers/ProductoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Producto::all(); // Listar productos
+    }
+
+    public function store(Request $request)
+    {
+        $producto = Producto::create($request->all());
+        return response()->json($producto, 201); // Crear producto
+    }
+
+    public function show($id)
+    {
+        $producto = Producto::findOrFail($id);
+        return response()->json($producto);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::findOrFail($id);
+        $producto->update($request->all());
+        return response()->json($producto);
+    }
+
+    public function destroy($id)
+    {
+        Producto::destroy($id);
+        return response()->json(null, 204);
+    }
+}
